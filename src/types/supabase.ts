78971f5abb94b5b2896d010e63917ecb5b86e1c7
Redgie Gravador <redgie.gravador@8x8.com>
2025-11-06 00:14:23 +0800

    redo

--- src/types/supabase.ts
+++
@@ -1,9 +0,0 @@
-// This will be auto-generated from Supabase CLI or defined manually
-export type Database = {
-  public: {
-    Tables: {
-      // Define your database schema here
-      [key: string]: any
-    }
-  }
-}
